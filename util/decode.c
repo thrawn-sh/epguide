3ce48b71cffce7451d2e9f6b9b33f8999b9c65c9
Alexander Dreweke <thrawn@shadowhunt.de>
2010-05-02 11:35:38 +0200

    remove nzb (was not working correct)

--- util/decode.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-void
-call_uudeview(char *filename)
-{
-        execlp(UUDEVIEW, "uudeview", "-i", "-c", "-d", "-q", filename, NULL);
-        execlp("/usr/bin/uudeview", "uudeview", "-i", "-c", "-d", "-q", filename, NULL);
-        die("%s", "unable to exec uudeview");
-}
