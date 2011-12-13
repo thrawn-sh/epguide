17d159b3c2ff9578e24aa8addf1e4e784b8d1c49
Thomas Glanzmann <thomas@glanzmann.de>
2011-12-13 19:40:17 +0100

    escape correctly and add missing ,

--- src/NZB/Binsearch.pm
+++ src/NZB/Binsearch.pm
@@ -52,7 +52,7 @@ sub searchNZB #{{{1
 				\>\ size:\ ([^,]*)                       # size
 				,\ parts\ available:.*? (\d+)\ \/\ (\d+) # parts_available parts_complete
 				(.*requires\ password.*)?                # password_required
-                                .*href=\"(https?:\/\/binsearch\.info\/viewNFO\.php\?[^"]+)" # nfo link
+                                .*href=\"(https?:\/\/binsearch\.info\/viewNFO\.php\?[^"]+)\" # nfo link
 				.*>([^<]+)<\/a><td><a                    # poster
 				//mxi
 			)
@@ -62,7 +62,7 @@ sub searchNZB #{{{1
 					$password = 1;
 				}
 				if ($4 == $5) {
-					my $nzb = { id => $1, subject => $2, size => $3, password=> $password, nfo => $7 poster => $8 };
+					my $nzb = { id => $1, subject => $2, size => $3, password=> $password, nfo => $7, poster => $8 };
 					push (@nzbs, $nzb);
 				}
 			}
