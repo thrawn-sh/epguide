a087550eade9fe2067567893e63a789dedb727e3
Alexander Dreweke <alexander@dreweke.net>
2015-10-13 23:37:35 +0200

    migrate to tvmaze

--- lib/EpisodeGuide/EpisodeGuides.pm
+++ lib/EpisodeGuide/EpisodeGuides.pm
@@ -5,12 +5,12 @@ package EpisodeGuide::EpisodeGuides;
 use strict;
 use warnings FATAL => 'all';
 
-use Date::Calc qw( Add_Delta_Days Today Date_to_Time Time_to_Date This_Year);
+use Date::Calc qw( Add_Delta_Days Today Date_to_Time Time_to_Date This_Year );
 use Date::Format;
 use Date::Parse;
+use JSON;
 use LWP::ConnCache;
 use Log::Log4perl;
-use Text::CSV;
 use WWW::Mechanize::GZip;
 
 my $LOGGER = Log::Log4perl->get_logger();
@@ -26,7 +26,6 @@ sub new {
     my %params = @_;
 
     my $www = WWW::Mechanize::GZip->new(autocheck => 1, ssl_opts => { verify_hostname => 0 });
-    $www->agent_alias('Windows IE 6');
     $www->conn_cache(LWP::ConnCache->new);
     $www->default_header('Accept-Language' => 'en');
 
@@ -44,11 +43,10 @@ sub getAllEpisodes($$$) { #{{{1
     my @episodes;
     my $today = Date_to_Time(Today(), 0, 0, 0);
     my $first = Date_to_Time(Add_Delta_Days(Today(), -90), 0, 0, 0);
-    my $year  = This_Year();
 
     $LOGGER->info('time periode: ' . $first . ' -> ' . $today);
 
-    my $url = 'http://epguides.com/common/exportToCSVmaze.asp?maze=' . $serieID;
+    my $url = 'http://api.tvmaze.com/shows/' . $serieID . '/episodes';
     $LOGGER->info('url: ' . $url);
 
     my $www = $self->{'www'};
@@ -58,53 +56,26 @@ sub getAllEpisodes($$$) { #{{{1
         return undef;
     }
 
-    my $csv = Text::CSV->new({ binary => 1, allow_loose_quotes => 1 });
-
-    for my $line (split(/\r|\n/, $www->text())) {
-        $line = trim($line);
-
-        next if ($line eq ""); # empty
-        next if ($line eq "list output"); # document title
-        next if ($line eq "number,season,episode,airdate,title,TVmaze link"); # csv header
-
-        unless ($csv->parse($line)) {
-            $LOGGER->info('Could not parse line: => skipping (' . $line . ')' . $csv->error_diag() );
-            next;
-        }
-
-        my @fields   = $csv->fields();
-        my $season   = $fields[1];
-        my $episode  = $fields[2];
-        my $released = $fields[3];
-
+    my $json = JSON->new->allow_nonref;
+    my $data = $json->decode($www->content(decoded_by_headers => 1));
+    foreach my $entry (@$data) {
+        my $season = $entry->{'season'};
         unless($season) {
-            $LOGGER->info('Missing season : => skipping (' . $line . ')');
+            $LOGGER->info('Missing season : => skipping');
             next;
         }
+
+        my $episode = $entry->{'number'};
         unless($episode) {
-            $LOGGER->info('Missing episode : => skipping (' . $line . ')');
+            $LOGGER->info('Missing episode : => skipping');
             next;
         }
+
+        my $released = $entry->{'airdate'};
         unless($released) {
-            $LOGGER->info('Missing release date : => skipping (' . $line . ')');
+            $LOGGER->info('Missing release date : => skipping');
             next;
         }
-        $released =~ s# #/#g;
-
-        my @dateparts = split(/\/| /,$released);
-        unless (scalar(@dateparts) == 3) {
-            $LOGGER->info('Incomplete release date : ' . $released . ' => skipping (' . $line . ')');
-            next;
-        }
-
-        if ($dateparts[2] < 100) {
-            $dateparts[2] += 2000;
-            if ($dateparts[2] > $year) {
-                $dateparts[2] -= 100;
-            }
-        }
-
-        $released = join('/', @dateparts);
         $released = str2time($released);
 
         if (($released > $first) && ($released < $today)) {
